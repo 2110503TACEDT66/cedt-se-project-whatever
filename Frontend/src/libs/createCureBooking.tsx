89ddd29fe9a9c8d45849ba24db10d58f8057893f
TrApDaCoda <134617720+TrApDaCoda@users.noreply.github.com>
2024-04-16 14:47:32 +0700

    create cureBooking libs

---
+++ Frontend/src/libs/createCureBooking.tsx
@@ -0,0 +1,35 @@
+'use server';
+import { revalidateTag } from 'next/cache';
+
+export default async function createBooking(
+  token: string,
+  user: string,
+  dentistId: string,
+  startDate: string,
+  endDate: string,
+  symptom: string
+) {
+  const response = await fetch(
+    `${process.env.BACKEND_URL}/api/v1/dentists/${dentistId}/bookings`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        user: user,
+        startDate: startDate,
+        endDate: endDate,
+        symptom: symptom,
+        reqType: 'cure',
+        status: 'pending',
+      }),
+    }
+  );
+  if (!response.ok) {
+    throw new Error(`Failed to create booking`);
+  }
+  revalidateTag('booking');
+  return await response.json();
+}
