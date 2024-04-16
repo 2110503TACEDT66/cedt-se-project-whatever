b82e1ad9cac4ad18d8533325469d10ae871b6658
Pitchaya Rueangsakpakdee <focustoplaner@hotmail.com>
2024-04-16 11:45:08 +0700

    add middleware path

--- Frontend/src/middleware.ts
+++ Frontend/src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/dentists/:path*', '/mybookings/'],
+  matcher: ['/dentists/:path*', '/mybookings/:path*'],
 };
