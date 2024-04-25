2088687b789da398566baa17a51f19c40477e443
Shiorashi <manpeakzaa@gmail.com>
2024-04-25 22:16:48 +0700

    page parameter in getFeedbackForOne

--- Frontend/src/libs/getFeedbackForOne.tsx
+++ Frontend/src/libs/getFeedbackForOne.tsx
@@ -1,5 +1,5 @@
-export default async function getFeedbackForOne(dentistId:string) {
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/dentists/${dentistId}/feedbacks`);
+export default async function getFeedbackForOne(dentistId:string, page:number) {
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/dentists/${dentistId}/feedbacks/?page=${page}`);
     if (!response.ok) {
       throw new Error('Failed to fetch feedbacks');
     }
