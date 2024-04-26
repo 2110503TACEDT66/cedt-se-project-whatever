f2267883fa69a7eda8ccfb778a2729f22a919d02
Shiorashi <manpeakzaa@gmail.com>
2024-04-26 14:26:51 +0700

    Add tailwind to ChangePage Button

--- Frontend/src/components/ShowFeedbacks.tsx
+++ Frontend/src/components/ShowFeedbacks.tsx
@@ -14,13 +14,15 @@ export default function ShowFeedback({dentistId}:{dentistId:string}) {
   return (
     <div className='space-y-4'>
       <div className='flex flex-row'>
-        {data.length == 0 ? null : <button className="rounded-lg bg-blue text-black p-3" onClick={() => {setPage(page+1);}}>
+        {data.length == 0 ? null : <button className="rounded-md bg-sky-600 hover:bg-sky-700 px-3 py-2
+            shadow-sm text-white" onClick={() => {setPage(page+1);}}>
           Next
         </button>}
         
-        <p className='text-black p-3'>{page}</p>
-        {page <= 1 ? null : <button className="rounded-lg bg-blue text-black p-3" onClick={() => {setPage(page-1);}}>
-          Previous
+        <p className='text-black px-6 py-3'>{page}</p>
+        {page <= 1 ? null : <button className="rounded-md bg-sky-600 hover:bg-sky-700 px-3 py-2
+            shadow-sm text-white" onClick={() => {setPage(page-1);}}>
+          Back
         </button>
         }
         </div>
