fb2ac3a665f0dfb668fc6d316fe5a30211b8bad8
Icegoo141 <isoonthipthanasup@gmail.com>
2024-04-29 21:07:36 +0700

    revert commit

--- Backend/models/Booking.js
+++ Backend/models/Booking.js
@@ -34,6 +34,10 @@ const BookingSchema = new mongoose.Schema({
     enum: ['checkup', 'cure'],
     required: true,
   },
+  commented: {
+    type: Boolean,
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
