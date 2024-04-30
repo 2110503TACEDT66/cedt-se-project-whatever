b33ec61ab256ffb47b8e84e90f149b78a1008b86
Pitchaya Rueangsakpakdee <focustoplaner@hotmail.com>
2024-05-01 01:40:39 +0700

    cypress till US4

--- Frontend/cypress/e2e/US4.cy.ts
+++ Frontend/cypress/e2e/US4.cy.ts
@@ -3,37 +3,9 @@ describe('US4', () => {
     cy.visit('/');
   });
 
-  // it('Login as User and sort the dentist’s list by area of expertise', () => {
-  //   cy.contains('My Booking').click()
-  //   cy.get('input[name="email"]').type('Number12345@gmail.com')
-  //   cy.get('input[name="password"]').type('12345678')
-  //   cy.contains('Sign in with Credentials').click()
-  //   cy.contains('Add new booking').click()
-  //   cy.get('#Bdate').click()
-  //   cy.contains('29').click()
-  //   cy.contains('OK').click()
-  //   cy.get('#expertise').parent().click().get('ul > li[data-value="Orthodontics"]').click()
-  //   expect('Simon Salmon').to.exist
-  // });
-
-  // it('Login as User and sort the dentist’s list by dentist experience', () => {
-  //   cy.contains('My Booking').click()
-  //   cy.get('input[name="email"]').type('Number12345@gmail.com')
-  //   cy.get('input[name="password"]').type('12345678')
-  //   cy.contains('Sign in with Credentials').click()
-  //   cy.contains('Add new booking').click()
-  //   cy.get('#Bdate').click()
-  //   cy.contains('29').click()
-  //   cy.contains('OK').click()
-  //   expect('Simon Salmon').to.exist
-  //   cy.get('#Sortexp').type("3")
-  //   cy.contains('Available Dentists').click()
-  //   cy.contains('Simon Salmon').should('not.exist');
-  // });
-
-  it('Login as Receptionist and sort the dentists list by area of expertise', () => {
+  it('Login as User and sort the dentist’s list by area of expertise', () => {
     cy.contains('My Booking').click()
-    cy.get('input[name="email"]').type('Rec4@gmail.com')
+    cy.get('input[name="email"]').type('Number12345@gmail.com')
     cy.get('input[name="password"]').type('12345678')
     cy.contains('Sign in with Credentials').click()
     cy.contains('Add new booking').click()
@@ -44,9 +16,9 @@ describe('US4', () => {
     expect('Simon Salmon').to.exist
   });
 
-  it('Login as Receptionist and sort the dentists list by dentists experience', () => {
+  it('Login as User and sort the dentist’s list by dentist experience', () => {
     cy.contains('My Booking').click()
-    cy.get('input[name="email"]').type('Rec4@gmail.com')
+    cy.get('input[name="email"]').type('Number12345@gmail.com')
     cy.get('input[name="password"]').type('12345678')
     cy.contains('Sign in with Credentials').click()
     cy.contains('Add new booking').click()
@@ -59,10 +31,31 @@ describe('US4', () => {
     cy.contains('Simon Salmon').should('not.exist');
   });
 
-  // it('Login as Receptionist and sort the dentist’s list by area of expertise', () => {
-  //   cy.contains('My Booking').click()
-  //   cy.get('input[name="email"]').type('Rec4@gmail.com')
-  //   cy.get('input[name="password"]').type('12345678')
-  //   cy.contains('Sign in with Credentials').click()
-  // });
+  it('Login as Receptionist and sort the dentists list by area of expertise', () => {
+    cy.contains('My Booking').click()
+    cy.get('input[name="email"]').type('Rec4@gmail.com')
+    cy.get('input[name="password"]').type('12345678')
+    cy.contains('Sign in with Credentials').click()
+    cy.visit('/dentists');
+    cy.contains('Available Dentists').should('not.exist');
+  });
+
+  it('Login as Receptionist and sort the dentists list by dentists experience', () => {
+    cy.contains('My Booking').click()
+    cy.get('input[name="email"]').type('Rec4@gmail.com')
+    cy.get('input[name="password"]').type('12345678')
+    cy.contains('Sign in with Credentials').click()
+    cy.visit('/dentists');
+    cy.contains('Available Dentists').should('not.exist');
+  });
+
+  it('Guess is not login and sort the dentists list by area of expertise', () => {
+    cy.contains('My Booking').click()
+    cy.contains('Simon Salmon').should('not.exist');
+  });
+
+  it('Guess is not login and sort the dentists list by dentists experience', () => {
+    cy.contains('My Booking').click()
+    cy.contains('Simon Salmon').should('not.exist');
+  });
 })
