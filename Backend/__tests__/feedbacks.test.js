6dc0679cb8adb9659f2fbcf9f9b5e1e43540b36f
Icegoo141 <isoonthipthanasup@gmail.com>
2024-05-04 18:13:42 +0700

    getFeedbacks and getFeedback

--- Backend/__tests__/feedbacks.test.js
+++ Backend/__tests__/feedbacks.test.js
@@ -1,4 +1,4 @@
-const { getFeedbacks } = require('../controllers/feedbacks');
+const { getFeedbacks, getFeedback } = require('../controllers/feedbacks');
 const Feedback = require('../models/Feedback');
 
 jest.mock('../models/Feedback');
@@ -9,6 +9,10 @@ const next = jest.fn();
 
 beforeEach(() => {
   req = { user: {}, params: {}, query: {}, body: {} };
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
 });
 
 describe('getFeedbacks', () => {
@@ -34,10 +38,31 @@ describe('getFeedbacks', () => {
     });
   });
 
-  it('should return a feedback with the feedbackId provided in the query', async () => {
-    req.query = { feedbackId: '111' };
+  it('should return a feedback with the bookingId provided in the query', async () => {
+    req.query = { bookingId: '420' };
 
-    Feedback.findById.mockResolvedValue([
+    Feedback.find.mockResolvedValue([
+      {
+        _id: '111',
+        user: '112',
+        dentist: '123',
+        booking: '420',
+      },
+    ]);
+
+    await getFeedbacks(req, res, next);
+
+    expect(res.status).toBeCalledWith(200);
+    expect(res.json).toBeCalledWith({
+      success: true,
+      count: 1,
+      averageRating: null,
+      data: [{ _id: '111', user: '112', dentist: '123', booking: '420' }],
+    });
+  });
+
+  it('should return all feedback when neither dentistId nor bookingId is provided', async () => {
+    Feedback.find.mockResolvedValue([
       {
         _id: '111',
         user: '112',
@@ -46,12 +71,71 @@ describe('getFeedbacks', () => {
     ]);
 
     await getFeedbacks(req, res, next);
+
     expect(res.status).toBeCalledWith(200);
     expect(res.json).toBeCalledWith({
       success: true,
       count: 1,
       averageRating: null,
       data: [{ _id: '111', user: '112', dentist: '123' }],
+    });
+  });
+
+  it('should response with status code 500 when something goes wrong', async () => {
+    Feedback.find.mockRejectedValue(new Error('Server down'));
+
+    await getFeedbacks(req, res, next);
+    expect(res.status).toBeCalledWith(500);
+    expect(res.json).toBeCalledWith({
+      success: false,
+      message: 'Cannot find feedbacks',
+    });
+  });
+});
+
+describe('getFeeedback', () => {
+  it('should return a feedback with the id provided', async () => {
+    req.params = { feedbackId: '123' };
+    Feedback.findById.mockResolvedValue([
+      {
+        _id: '123',
+        user: '111',
+        dentist: '112',
+      },
+    ]);
+
+    await getFeedback(req, res, next);
+
+    expect(res.status).toBeCalledWith(200);
+    expect(res.json).toBeCalledWith({
+      success: true,
+      data: [{ _id: '123', user: '111', dentist: '112' }],
+    });
+  });
+
+  it('should return a status code 404 when there is no feedback with the id provided', async () => {
+    req.params = { feedbackId: '123' };
+    Feedback.findById.mockResolvedValue(null);
+
+    await getFeedback(req, res, next);
+
+    expect(res.status).toBeCalledWith(404);
+    expect(res.json).toBeCalledWith({
+      success: false,
+      message: 'No feedback with the id of 123',
+    });
+  });
+
+  it('should response with status code 500 when something goes wrong', async () => {
+    req.params = { feedbackId: '123' };
+    Feedback.findById.mockRejectedValue(new Error('Server down'));
+
+    await getFeedback(req, res, next);
+
+    expect(res.status).toBeCalledWith(500);
+    expect(res.json).toBeCalledWith({
+      success: false,
+      message: 'Cannot find feedback',
     });
   });
 });
